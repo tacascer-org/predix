711a7d2b572b5040fe70979c47a5438a0d078cbc
Tim Tran <tdt46@cornell.edu>
2023-03-11 22:07:56 -0500

    build: Change to Kotlin

---
+++ src/test/kotlin/com/github/tacascer/predix/PredixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.tacascer.predix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PredixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
