f4597397fa53533f8eba86b0443661a2b3132f43
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-01-11 22:19:09 -0500

    chore: Add actuator (#48)
    * chore: Add actuator
    
    * build: Deprecate buildDir usage
    
    * ci: Use build scan
    
    * build: Use build scan

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,14 +24,16 @@ repositories {
 
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.testcontainers:junit-jupiter")
     testImplementation("org.testcontainers:mongodb")
 }
@@ -56,6 +58,6 @@ sonar {
         property("sonar.projectKey", "tacascer_predix")
         property("sonar.organization", "tim-tran")
         property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.coverage.jacoco.xmlReportPaths", "${buildDir}/reports/kover/report.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "${layout.buildDirectory}/reports/kover/report.xml")
     }
 }
