38788f2ac2a0a88a70821cef2bf3a1a740fed097
Tim Tran <49887921+tacascer@users.noreply.github.com>
2024-01-16 00:30:57 -0500

    build: Add H2 for now

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework:spring-jdbc")
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("io.r2dbc:r2dbc-h2")
     testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
     testImplementation("io.projectreactor:reactor-test")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
